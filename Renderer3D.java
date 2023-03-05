82cdf8083401326e62b4f56173afefccdfb39f78
Rami Elsayed <ramielsayed965@gmail.com>
2023-03-05 10:12:53 -0600

    Broken ZBuffer

--- Renderer3D.java
+++ Renderer3D.java
@@ -7,7 +7,7 @@ import java.awt.image.BufferedImage;
 import java.util.Scanner;
 
 // TODO:
-// Fix some weirdness: ONE TRIANGLE ALWAYS ON TOP
+// Fix weirdness with Depth Buffer
 // Add Shading
 // Add More Shapes
 
@@ -94,8 +94,17 @@ public class Renderer3D {
                     0, Math.cos(vertical), Math.sin(vertical),
                     0, -Math.sin(vertical), Math.cos(vertical)
                 });
+                
                 Matrix3 transform = horizontalTransform.multiply(verticalTransform);
                 BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+                
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+                // initialize array with extremely far away depths
+                for (int q = 0; q < zBuffer.length; q++) {
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
+                
+                
                 // Draw Loop
                 for (Triangle t : shape) {
                     Vertex v1 = transform.transform(t.v1);
@@ -123,7 +132,12 @@ public class Renderer3D {
                             double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
                             double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
                             if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) { // what
-                                img.setRGB(x, y, t.color.getRGB());
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                int zIndex = y * img.getWidth() + x;
+                                if (zBuffer[zIndex] < depth) {
+                                    img.setRGB(x, y, t.color.getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
                             }
                         }
                     }
